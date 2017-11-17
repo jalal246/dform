4936aa53cc9033778ae50ab7f753c2b8bf7be586
jimmy <jimmy002020@gmail.com>
2017-11-17 16:28:14 +0200

    add eslint rules to stortbook

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+// import { linkTo } from '@storybook/addon-links'
 
-import { Button, Welcome } from '@storybook/react/demo';
+// import { Button, Welcome } from '@storybook/react/demo'
+import DevForm from '../src'
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// storiesOf('Welcome', module).add('to Storybook', () => (
+//   <Welcome showApp={linkTo('Button')} />
+// ))
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf('dynamic form', module).add('main component', () => <DevForm />)
+// .add('with text', () => (
+//   <Button onClick={action('clicked')}>Hello Button</Button>
+// ))
+// .add('with some emoji', () => (
+//   <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+// ))
