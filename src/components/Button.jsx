4ac2959185d2d5d09d0db3ec41fd3d929769c4b7
jimmy <jimmy002020@gmail.com>
2017-11-18 14:51:38 +0200

    add defaultProps and name tag. Change prop opt to dataOpt

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,25 +1,30 @@
-/* eslint react/require-default-props: 0 */
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
 class Button extends PureComponent {
   static propTypes = {
-    opt: PropTypes.string.isRequired,
-    isRw: PropTypes.string,
     type: PropTypes.string,
-    txt: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
+    name: PropTypes.string,
+    onClick: PropTypes.func.isRequired,
+    dataOpt: PropTypes.string,
+    txt: PropTypes.string.isRequired,
+  }
+  static defaultProps = {
+    type: 'button',
+    disabled: false,
+    name: 'btn',
+    dataOpt: 'btn',
   }
   render() {
-    const { opt, isRw, disabled, txt, type, onClick } = this.props
+    const { type, disabled, name, dataOpt, txt, onClick } = this.props
     return (
       <button
-        type={type || 'button'}
+        type={type}
         disabled={disabled}
+        name={name}
         onClick={onClick}
-        data-opt={opt}
-        data-rw={isRw}
+        data-opt={dataOpt}
       >
         {txt}
       </button>
