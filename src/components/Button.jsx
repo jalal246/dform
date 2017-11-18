11d4be22a93c34b85029f213402123aada3c2b55
jimmy <jimmy002020@gmail.com>
2017-11-18 19:59:00 +0200

    change to func

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,35 +1,31 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Button extends PureComponent {
-  static propTypes = {
-    type: PropTypes.string,
-    disabled: PropTypes.bool,
-    onClick: PropTypes.func.isRequired,
-    name: PropTypes.string,
-    dataOpt: PropTypes.string,
-    txt: PropTypes.string.isRequired,
-  }
-  static defaultProps = {
-    type: 'button',
-    disabled: false,
-    name: null,
-    dataOpt: null,
-  }
-  render() {
-    const { type, disabled, name, dataOpt, txt, onClick } = this.props
-    return (
-      <button
-        type={type}
-        disabled={disabled}
-        onClick={onClick}
-        name={name}
-        data-opt={dataOpt}
-      >
-        {txt}
-      </button>
-    )
-  }
+const Button = ({ type, disabled, name, dataOpt, txt, onClick }) => (
+  <button
+    type={type}
+    disabled={disabled}
+    onClick={onClick}
+    name={name}
+    data-opt={dataOpt}
+  >
+    {txt}
+  </button>
+)
+
+Button.propTypes = {
+  type: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  name: PropTypes.string,
+  dataOpt: PropTypes.string,
+  txt: PropTypes.string.isRequired,
+}
+Button.defaultProps = {
+  type: 'button',
+  disabled: false,
+  name: null,
+  dataOpt: null,
 }
 
 export default Button
